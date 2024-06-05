15b2fbfaf92c976948fe880c2be91ed191f6d072
R-utujatate <156331583+R-utujatate@users.noreply.github.com>
2024-06-05 22:50:31 +0530

    Add files via upload

---
+++ sum_of_2_no.java
@@ -0,0 +1,12 @@
+class Addition
+{
+	public static void main(String a[])
+	{
+		System.out.println("Enter the two numbers:");
+		int a=20,b=30;
+		int c=a+b;
+		System.out.println("value of a :"+a);
+		System.out.println("value of b :"+b);
+		System.out.println("addition is:"+c);
+	}
+}
