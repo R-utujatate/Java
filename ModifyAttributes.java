cbe1058c35805f6fc6e0ee2273777ea52054632d
R-utujatate <156331583+R-utujatate@users.noreply.github.com>
2024-07-23 18:49:57 +0530

    Add files via upload

---
+++ ModifyAttributes.java
@@ -0,0 +1,9 @@
+public class ModifyAttributes {
+    int x;
+    public static void main(String[] args) {
+        ModifyAttributes Obj=new ModifyAttributes();
+        Obj.x=40;
+        System.out.println(Obj.x);
+    }
+    
+}
